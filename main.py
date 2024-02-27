e0c3b6852231da76d3fed992dd2572918366935c
Dushan Senadheera <dcsenadheera777@gmail.com>
2024-02-27 17:10:06 +0530

    fix pagination

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import csv
 import time
-from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://www.tripadvisor.com/Attractions-g297896-Activities-oa0-Galle_Galle_District_Southern_Province.html")
+time.sleep(2)  # wait for the page to load
 
-with open("location_pg1.csv", "w", newline="") as csvfile:
+with open("destination.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Title", "Rating"])
+    writer.writerow(["Title", "Rating", "Review", "Category"])
 
-
-    time.sleep(2)  # wait for the page to load
+    #while True:
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, "html.parser")
 
     titles = soup.findAll("div", class_="XfVdV o AIbhI")
-    ratings = soup.findAll("svg", class_="UctUV d H0 hzzSG")['aria-label']
-    reviews = soup.findAll("span", class_="biGQs _P pZUbB osNWb")
-    categories = soup.findAll("div", class_="biGQs _P pZUbB hmDzD")
-
-    # see = driver.find_element(By.CLASS_NAME, "rmyCe _G B- z _S c Wc wSSLS pexOo sOtnj")
-    # see.click()
+    ratings = [svg.get('aria-label') for svg in soup.findAll("svg", class_="UctUV d H0 hzzSG")]
+    reviews = soup.findAll("span", class_="biGQs _P pZUbB osNWb")  # replace with the actual class name
+    categories = soup.findAll("div", class_="biGQs _P pZUbB alXOW oCpZu GzNcM nvOhm UTQMg ZTpaU W KxBGd")
 
     for title, rating, review, category in zip(titles, ratings, reviews, categories):
-        title_text = title.text
+        title_text = titles.text
         rating_text = ratings.text
-        review_text = review.text
-        categories_text = category
-        writer.writerow([title_text, rating_text, review_text, categories_text])
+        review_text = reviews.text
+        category_text = categories.text
+        writer.writerow([title_text, rating_text, review_text, category_text])
 
-        # next_button = driver.find_element(By.CLASS_NAME, "BrOJk u j z _F wSSLS tIqAi unMkR")
-        # next_button.click()
+        # try:
+        #     next_button_xpath = """//*[@id="lithium-root"]/main/div[1]/div/div[3]/div/div[2]/div[2]/div[2]/div/div/div/div[2]/div/div[2]/div/div/section[40]/div/div[1]/div/div[1]/div[2]/div"""  # replace with the actual XPath of the next button
+        #     next_page = driver.find_element(By.XPATH, next_button_xpath)
+        #     next_page.click()
+        #     time.sleep(2)  # wait for the new page to load
+        # except Exception as e:
+        #     print("Scraping finished.")
+        #     break
 
-
-
-print("finished web scraping")
+print("Finished web scraping")
 driver.quit()

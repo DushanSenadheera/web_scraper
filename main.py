c50b11c9b02e3752449b3ccc4674bc9640d2e65f
Dushan Senadheera <dcsenadheera777@gmail.com>
2024-02-28 03:33:06 +0530

    optimize the code

--- main.py
+++ main.py
@@ -6,36 +6,43 @@ import time
 
 driver = webdriver.Chrome()
 driver.get("https://www.tripadvisor.com/Attractions-g297896-Activities-oa0-Galle_Galle_District_Southern_Province.html")
-time.sleep(2)  # wait for the page to load
+time.sleep(2)
 
-with open("destination.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Title", "Rating", "Review", "Category"])
+page_source = driver.page_source
+soup = BeautifulSoup(page_source, "html.parser")
 
-    #while True:
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, "html.parser")
+titles = soup.findAll("div", class_="XfVdV o AIbhI")
+ratings = [svg.get('aria-label') for svg in soup.findAll("svg", class_="UctUV d H0 hzzSG")]
+reviews = soup.findAll("span", class_="biGQs _P pZUbB osNWb")
+categories = soup.findAll("div", class_="biGQs _P pZUbB hmDzD")
 
-    titles = soup.findAll("div", class_="XfVdV o AIbhI")
-    ratings = [svg.get('aria-label') for svg in soup.findAll("svg", class_="UctUV d H0 hzzSG")]
-    reviews = soup.findAll("span", class_="biGQs _P pZUbB osNWb")  # replace with the actual class name
-    categories = soup.findAll("div", class_="biGQs _P pZUbB alXOW oCpZu GzNcM nvOhm UTQMg ZTpaU W KxBGd")
+csvfile = open("destination.csv", "w", newline="")
+writer = csv.writer(csvfile)
+writer.writerow(["Title", "Rating", "Review", "Category"])
 
-    for title, rating, review, category in zip(titles, ratings, reviews, categories):
-        title_text = titles.text
-        rating_text = ratings.text
-        review_text = reviews.text
-        category_text = categories.text
-        writer.writerow([title_text, rating_text, review_text, category_text])
+rows = [[title.text, rating.text, review.text, category.text] for title, rating, review, category in
+        zip(titles, ratings, reviews, categories)]
+writer.writerows(rows)
 
-        # try:
-        #     next_button_xpath = """//*[@id="lithium-root"]/main/div[1]/div/div[3]/div/div[2]/div[2]/div[2]/div/div/div/div[2]/div/div[2]/div/div/section[40]/div/div[1]/div/div[1]/div[2]/div"""  # replace with the actual XPath of the next button
-        #     next_page = driver.find_element(By.XPATH, next_button_xpath)
-        #     next_page.click()
-        #     time.sleep(2)  # wait for the new page to load
-        # except Exception as e:
-        #     print("Scraping finished.")
-        #     break
+csvfile.close()
 
-print("Finished web scraping")
+# with open("destination.csv", "w", newline="") as csvfile:
+#     writer = csv.writer(csvfile)
+#     writer.writerow(["Title", "Rating", "Review", "Category"])
+#     print('csv')
+
+#     for title, rating, review, category in zip(titles, ratings, reviews, categories):
+#         print('loop inside')
+#         writer.writerow([title.text, rating, review.text, category.text])
+
+# try:
+#     next_button_xpath = """//*[@id="lithium-root"]/main/div[1]/div/div[3]/div/div[2]/div[2]/div[2]/div/div/div/div[2]/div/div[2]/div/div/section[40]/div/div[1]/div/div[1]/div[2]/div"""  # replace with the actual XPath of the next button
+#     next_page = driver.find_element(By.XPATH, next_button_xpath)
+#     next_page.click()
+#     time.sleep(2)  # wait for the new page to load
+# except Exception as e:
+#     print("Scraping finished.")
+#     break
+
 driver.quit()
+print("Finished web scraping")

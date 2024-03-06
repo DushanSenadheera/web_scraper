5910523863345bca3da974e1e2947cd5b2f8cad8
Dushan Senadheera <dcsenadheera777@gmail.com>
2024-03-06 20:12:08 +0530

    fix tab scrape error

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import csv
 import time
@@ -12,11 +16,9 @@ def web_scraper(driver, writer):
     soup = BeautifulSoup(page_source, "html.parser")
 
     titles = soup.findAll("div", class_="XfVdV o AIbhI")
-    #ratings = soup.findAll("svg", class_="UctUV d H0 hzzSG")
     reviews = soup.findAll("span", class_="biGQs _P pZUbB osNWb")
     categories = soup.findAll("div", class_="biGQs _P pZUbB hmDzD")
 
-    
     for title, review, category in zip(titles, reviews, categories):
         original_tab = driver.current_window_handle
         title_link = driver.find_element(By.LINK_TEXT, title.text)
@@ -26,28 +28,38 @@ def web_scraper(driver, writer):
         new_tab = [tab for tab in driver.window_handles if tab != original_tab][0]
         driver.switch_to.window(new_tab)
 
-        details = driver.find_element(By.CLASS_NAME, "JguWG").text
-        rating = driver.find_element(By.CLASS_NAME, "jVDab o W f u w GOdjs")
-        location = driver.find_element(By.CLASS_NAME, "biGQs _P pZUbB avBIb osNWb").text
-        duration = driver.find_element(By.CLASS_NAME, "biGQs _P pZUbB KxBGd").text
-        
+        wait = WebDriverWait(driver, 10)
+        details = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[2]/div/div/div/div[1]/div[1]/div/div[2]/div/div[1]/div/span")))
+        location = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div/main/div[1]/div[1]/div/div/div[5]/a/span/span")))
+        rating = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[1]/div/div/div/div/div[1]/div[1]/a/div")))
+        duration = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div[1]/div[2]/div[2]/div[2]/div/div[1]/section[2]/div/div/div/div[1]/div[1]/div/div[3]/div[2]/div/div")))
+
+        # Extract the details
+        details_text = details.text
+        location_text = location.text
+        rating_text = rating.text
+        duration_text = duration.text
+         
+        writer.writerow([title.text, rating_text, review.text, category.text, details_text, location_text, duration_text])
+
         driver.close()
         time.sleep(random.randint(5, 15))
 
-        driver.switch_to.window(original_tab)
-        writer.writerow([title.text, rating.get_attribute("aria-label"), review.text, category.text, details, location, duration])
+        driver.switch_to.window(original_tab)   
 
 
 def main():
     options = Options()
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get("https://www.tripadvisor.com/Attractions-g2467810-Activities-oa0-Southern_Province.html")
     time.sleep(random.randint(5, 15))
 
-    with open("test.csv", "a", newline="", encoding='utf-8') as csvfile:
+    with open("destination.csv", "a", newline="", encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["Title", "Rating", "Review", "Category", "About", "Location", "Duration"])
 
